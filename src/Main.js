14f1468aafde3a60b0cc99c7a9158e3aac7aa7ba
Enkai <enkaihuang2021@gmail.com>
2023-03-01 20:12:46 +0800

    fix some bugs

--- src/Main.js
+++ src/Main.js
@@ -5,20 +5,20 @@ export default function Main() {
     return (
         <div className="main">
             <nav className="header">
-                <img src={Cover} alt="logo" className="header--logo" />
-                <h2 className="header--title">Hololive</h2>
-                <h3 className="home">Home</h3>
-                <h3 className="about_us">About us</h3>
-                <h3 className="contact">Contact</h3>
+                <div className="navbar">
+                    <img src={Cover} alt="logo" className="header--logo" />
+                    <h2 className="header--title">Hololive</h2>
+                    <div className="nav-links">
+                        <h3>Home</h3>
+                        <h3>About us</h3>
+                        <h3>Contact</h3>
+                    </div>
+                </div>
             </nav>
-            <h1 className="main--title1">
-                Welcome to
+
+            <h1 className="main--title">
+                Welcome to <br/> Hololive Encyclopedia
             </h1>
-            <h2 className="main--title2">
-                Hololive Encyclopedia
-            </h2>
-
-
         </div>
     );
 }
