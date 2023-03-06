4e5462256b484e67fa1e3182b28422224c101a0c
Enkai <enkaihuang2021@gmail.com>
2023-03-06 17:26:54 +0800

    fix cut-corner bug

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -5,11 +5,11 @@ export default function Intro() {
     return (
         <div className="intro" id="intro">
             {/* <div class="cut-corner">Some content</div> */}
-            <div class="cut-corner filled">Some content</div>
+            <div class="cut-corner">Some content</div>
 
             <div className="intro--title--square"></div>
             {/* <div className="intro--title--trapezoidal"></div> */}
-            <div className="intro--title">誰でも大好き</div>
+            <div className="intro--title">誰�?��??大好???</div>
         </div>
     );
 }
