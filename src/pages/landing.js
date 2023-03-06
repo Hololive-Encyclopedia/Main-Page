9f086ab06508706f8e0ce238ea4204bd5a1eb2fd
BangDonKiller <parisdata@gmail.com>
2023-03-07 00:00:25 +0800

    fix button

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -3,13 +3,14 @@ import Cover from "../image/cover.png";
 import Menu from "./Menu";
 
 export default function Landing() {
+
     return (
         <div className="main">
             <nav className="header">
                 <div className="navbar">
                     <img src={Cover} alt="logo" className="header--logo" />
                     <h2 className="header--title">Hololive</h2>
-                    <Menu/>
+                    <Menu />
                     <div className="nav-links">
                         <a href="#home">Home</a>
                         <a href="#about">About us</a>
