b9cd1c6c2a28e19fd6bcfde756ab2f5a1ac0dbfa
BangDonKiller <parisdata@gmail.com>
2023-03-06 17:16:28 +0800

    wrong graph

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import '../intro.css';
 
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            <div className="intro--text">
-                <h1 className="intro--title">Hololive</h1>
-                <p className="intro--paragraph">
-                    Hololive is a virtual talent agency and YouTube channel created by Cover Corp. in 2018. The agency is a subsidiary of Cover Corp. and is managed by Cover Corp. CEO Kenichiro Yoshida. The agency's main focus is to create and manage virtual YouTubers, who are animated characters that are controlled by real people. The agency has a total of 30 virtual YouTubers, with 17 of them being active at the moment. The agency's main focus is to create and manage virtual YouTubers, who are animated characters that are controlled by real people. The agency has a total of 30 virtual YouTubers, with 17 of them being active at the moment. The agency's main focus is to create and manage virtual YouTubers, who are animated characters that are controlled by real people. The agency has a total of 30 virtual YouTubers, with 17 of them being active at the moment.
-                </p>
-            </div>
+            {/* <div class="cut-corner">Some content</div> */}
+            <div class="cut-corner filled">Some content</div>
+
+            <div className="intro--title--square"></div>
+            {/* <div className="intro--title--trapezoidal"></div> */}
+            <div className="intro--title">誰でも大好き</div>
         </div>
     );
 }
