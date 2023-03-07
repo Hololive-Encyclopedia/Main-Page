87d157c8383805de4a143cd0d13d0b7f37cc12c2
Enkai <enkaihuang2021@gmail.com>
2023-03-07 15:24:50 +0800

    add animation on down button on main page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,32 +1,78 @@
+// import React from 'react';
+// import Cover from "../image/cover.png";
+// import Menu from "./Menu";
+
+// export default function Landing() {
+
+//     return (
+//         <div className="main">
+//             <nav className="header">
+//                 <div className="navbar">
+//                     <img src={Cover} alt="logo" className="header--logo" />
+//                     <h2 className="header--title">Hololive</h2>
+//                     <Menu />
+//                     <div className="nav-links">
+//                         <a href="#home">Home</a>
+//                         <a href="#about">About us</a>
+//                         <a href="#contact">Contact</a>
+//                     </div>
+//                 </div>
+//             </nav>
+
+//             <h1 className="main--title">
+//                 Welcome to <br /> Hololive Encyclopedia
+//             </h1>
+//             <div className="down-button">
+//                 <a href="#intro">
+//                     <i class='fa fa-chevron-circle-down'></i>
+//                 </a>
+//             </div>
+//         </div>
+//     );
+// }
+
+
+
 import React from 'react';
 import Cover from "../image/cover.png";
 import Menu from "./Menu";
 
 export default function Landing() {
+  const scrollToIntro = (event) => {
+    event.preventDefault();
 
-    return (
-        <div className="main">
-            <nav className="header">
-                <div className="navbar">
-                    <img src={Cover} alt="logo" className="header--logo" />
-                    <h2 className="header--title">Hololive</h2>
-                    <Menu />
-                    <div className="nav-links">
-                        <a href="#home">Home</a>
-                        <a href="#about">About us</a>
-                        <a href="#contact">Contact</a>
-                    </div>
-                </div>
-            </nav>
+    const introSection = document.querySelector('#intro');
+    const introSectionPos = introSection.getBoundingClientRect().top + window.scrollY;
 
-            <h1 className="main--title">
-                Welcome to <br /> Hololive Encyclopedia
-            </h1>
-            <div className="down-button">
-                <a href="#intro">
-                    <i class='fa fa-chevron-circle-down'></i>
-                </a>
-            </div>
+    window.scrollTo({
+      top: introSectionPos,
+      behavior: 'smooth'
+    });
+  };
+
+  return (
+    <div className="main">
+      <nav className="header">
+        <div className="navbar">
+          <img src={Cover} alt="logo" className="header--logo" />
+          <h2 className="header--title">Hololive</h2>
+          <Menu />
+          <div className="nav-links">
+            <a href="#home">Home</a>
+            <a href="#about">About us</a>
+            <a href="#contact">Contact</a>
+          </div>
         </div>
-    );
+      </nav>
+
+      <h1 className="main--title">
+        Welcome to <br /> Hololive Encyclopedia
+      </h1>
+      <div className="down-button">
+        <a href="#intro" onClick={scrollToIntro}>
+          <i className='fa fa-chevron-circle-down'></i>
+        </a>
+      </div>
+    </div>
+  );
 }
